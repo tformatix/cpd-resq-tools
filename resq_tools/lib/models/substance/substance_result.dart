d26b5b4da17931eba7d842b5803bedf21a77aac1
Sebastian Weidlinger <s.weidlinger99@gmx.at>
2025-04-04 23:19:48 +0200

    feat: add substance result data class

---
+++ resq_tools/lib/models/substance/substance_result.dart
@@ -0,0 +1,13 @@
+class SubstanceResult {
+  final String name;
+  final int hazardNumber;
+  final int unNumber;
+  final String properties;
+
+  const SubstanceResult({
+    required this.name,
+    required this.hazardNumber,
+    required this.unNumber,
+    required this.properties,
+  });
+}
