12dd5338ed14a46688e6a71f65b1f3d831ecf2fc
Sebastian Weidlinger <83772339+SWeidlinger@users.noreply.github.com>
2025-04-05 13:19:14 +0200

    GH-4 Implement basic substance screen
    * fix: localization typo
    
    * feat: add substance_screen localizations
    
    * feat: add substance result data class
    
    * feat: implement basic dummy substance_screen
    
    * chore: resolve PR comments

--- resq_tools/lib/repositories/substance_repository.dart
+++ resq_tools/lib/repositories/substance_repository.dart
@@ -1,5 +1,18 @@
+import 'package:resq_tools/models/substance/substance_result.dart';
+
 class SubstanceRepository {
-  String getDummyString() {
-    return 'I am from SubstanceRepository';
+  Future<SubstanceResult?> fetchSubstance() async {
+    try {
+      //TODO: make network request
+      await Future.delayed(const Duration(seconds: 1));
+      return SubstanceResult(
+        name: 'Benzin',
+        hazardNumber: 33,
+        unNumber: 1203,
+        properties: 'Extrem entzündbare Flüssigkeit',
+      );
+    } catch (e) {
+      return null;
+    }
   }
 }
