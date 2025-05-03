0363062c679c09b5df633e1550864ac0ac3e0e12
Michael Zauner <mzauner98@gmail.com>
2025-05-03 18:23:34 +0200

    GH-9 Add calculation of resistance

--- resq_tools/lib/repositories/resistance_repository.dart
+++ resq_tools/lib/repositories/resistance_repository.dart
@@ -4,10 +4,19 @@ import 'package:resq_tools/models/resistance/measurement_config.dart';
 import 'package:resq_tools/models/resistance/resistance_result.dart';
 
 class ResistanceRepository {
-  ResistanceResult? calculateResistance(MeasurementConfig? measurementConfig) =>
-      ResistanceResult(
-          angle: measurementConfig?.angle ?? 0.0,
-          rollingResistance: Random().nextDouble() * 100,
-          gradientResistance: Random().nextDouble() * 100,
-          overallResistance: Random().nextDouble() * 2000);
+  ResistanceResult? calculateResistance(MeasurementConfig? measurementConfig) {
+    final rollingResistance =
+        (measurementConfig?.undergroundType.rollingResistanceCoefficient ??
+            0.0) *
+        (measurementConfig?.weight ?? 0.0);
+    final gradientResistance =
+        (measurementConfig?.weight ?? 0.0) *
+        sin(measurementConfig?.angle ?? 0.0 * pi / 180.0);
+
+    return ResistanceResult(
+      rollingResistance: rollingResistance,
+      gradientResistance: gradientResistance,
+      overallResistance: rollingResistance + gradientResistance,
+    );
+  }
 }
