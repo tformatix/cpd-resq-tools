8e1c422944ce8df56efc9447f4f0c36f32611f2d
Tobias Fischer <tobias@tformatix.at>
2025-05-12 13:17:35 +0200

    GH-42 Add more detailed regex for licence plates

--- resq_tools/lib/models/common/camera_ocr_type.dart
+++ resq_tools/lib/models/common/camera_ocr_type.dart
@@ -1,7 +1,10 @@
 enum CameraOcrType {
-  licensePlate(r'[A-Z]{1,3}[\s\-]?\d{1,5}[\s\-]?[A-Z]{1,3}'),
+  licensePlate(
+    r'^(AM|BA|BD|BH|BL|BM|BN|BP|BR|BZ|B|DL|DO|EF|EU|E|FE|FK|FR|FV|FW|GB|GD|GF|GM|GR|GS|GU|G|HA|HE|HF|HL|HO|IL|IM|I|JE|JO|JW|KB|KG|KI|KL|KO|KR|KS|KU|K|LA|LB|LE|LF|LI|LL|LN|LZ|L|MA|MD|ME|MI|MT|MU|ND|NK|OP|OW|PE|PL|PT|P|RE|RI|RO|SB|SD|SE|SL|SO|SP|SR|SV|SW|SZ|S|TA|TU|UU|VB|VI|VK|VL|VO|WB|WE|WL|WN|WO|WT|WY|WZ|W|ZE|ZT)[\s\-]?((?:[0-9]{1,5}[A-Z]{1,3})|(?:[A-Z]{1,5}[0-9]{1,5}))$', // ignore: lines_longer_than_80_chars
+  ),
   substance(r'^\d{4}$');
 
   final String regex;
+
   const CameraOcrType(this.regex);
 }
