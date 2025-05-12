ba5cb7f33396029563103605927deb271343d4f9
Sebastian Weidlinger <83772339+SWeidlinger@users.noreply.github.com>
2025-05-12 13:34:29 +0200

    GH-29: create custom app theme (#65)
    * GH-29: add dark theme
    
    * GH-29: add dark theme
    
    * GH-29: fix linting issue

---
+++ resq_tools/lib/theme/resq_tools_theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ResQToolsTheme {
+  static final ThemeData lightTheme = ThemeData(
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.red,
+    ),
+    useMaterial3: true,
+  );
+
+  static final ThemeData darkTheme = ThemeData(
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.red,
+      brightness: Brightness.dark,
+    ),
+    useMaterial3: true,
+  );
+}
